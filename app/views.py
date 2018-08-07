56742c420a1b3bb40c2fc4399658e7d7e64e8218
cenv0594 <gwyneth.bradbury@ouce.ox.ac.uk>
2018-08-07 12:27:56 +0100

    -

--- app/views.py
+++ app/views.py
@@ -171,7 +171,8 @@ def survey_2():
             and not Survey2.has_been_done_by(current_user.uid_trim(),datetime.datetime.utcnow().year)[0] :
 
         form = Survey2Form(request.form)
-        form.validate()
+        print(form.validate())
+        print(form.errors)
         if form.validate_on_submit():
 
             survey = ThirdPartyRegister(current_user.uid_trim(),alt_email="not a real email")
